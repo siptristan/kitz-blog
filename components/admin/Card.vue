7a7951a52c58df79976faa91322790713da1c818
siptristan <sip.tristan@hotmail.com>
2022-10-25 15:31:03 +0700

    update admin

---
+++ components/admin/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card w-40 h-fit px-2 py-2">
+        <img :src="require(`../../assets/icons/${source}`)" class="mb-4" alt="">
+        <h1 class="font-semibold text-md">12</h1>
+        <p class="text-sm">{{ desc }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Card',
+    props: {
+        source: {
+            type: String,
+            required: true,
+            default: 'home.svg'
+        },
+        desc: {
+            type: String,
+            required: true,
+            default: 'Data Not Found'
+        }
+    }
+}
+</script>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import SideBar from '../components/admin/SideBar.vue'
 import Header from '../components/admin/Header.vue'
+import ChartArea from '../components/admin/charts/ChartArea.vue'
+import Card from '../components/admin/Card.vue'
 
 export {
     SideBar,
-    Header
+    Header,
+    ChartArea,
+    Card
 }
