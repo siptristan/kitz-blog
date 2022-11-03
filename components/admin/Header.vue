1d9529183307e3680c94b19b69f42a530607ff7d
siplancelot <sip.lancelot@gmail.com>
2022-11-03 13:44:47 +0700

    responsive admin and repair layout admin

--- components/admin/Header.vue
+++ components/admin/Header.vue
@@ -18,27 +18,76 @@
     </div>
 
     <!-- Sidebar Mobile -->
-    <div v-bind:class="{ hidden: !showMenu, block: showMenu }" class="sidebar w-9/12 lg:hidden z-10 -mt-12 absolute">
-      <div class="w-11/12 mx-auto">
-        <div class="flex place-content-between">
-          <img src="../../assets/images/logo2.png" class="w-2/4 h-fit my-4" alt="">
-          <button class="" type="button">
-            <img src="../../assets/icons/close-button.svg" v-on:click="toggleNavbar()" alt="" />
-          </button>
+    <div v-bind:class="{ hidden: !showMenu, block: showMenu }" class="sidebar lg:hidden -mt-16 -ml-4 md:-ml-10 z-10 fixed">
+      <div class="flex w-10/12 mx-auto place-content-between">
+        <img src="../../assets/images/logo2.png" class="w-2/4 h-fit my-4" alt="">
+        <button class="" type="button">
+          <img src="../../assets/icons/close-button.svg" v-on:click="toggleNavbar()" alt="" />
+        </button>
+      </div>
+      <div class="flex flex-col justify-center items-center w-full h-48">
+        <div class="profile w-fit h-fit rounded-full border-sky-400 border-2 px-1 py-1">
+          <img src="../../assets/images/profile.jpg" class="w-20 h-20 rounded-full" alt="">
         </div>
-        <p class="font-semibold text-white">Dashboard</p>
-        <div class="flex mt-4">
-          <img src="../../assets/images/user.svg" class="mr-2" alt="">
-          <h3 class="text-white text-sm">Profile</h3>
-        </div>
-        <div class="flex mt-4">
-          <img src="../../assets/images/home.svg" class="mr-2" alt="">
-          <h3 class="text-white text-sm">Create Blog</h3>
-        </div>
-        <div class="flex mt-4">
-          <img src="../../assets/images/edit.svg" class="mr-2" alt="">
-          <h3 class="text-white text-sm">Manage Existing Blog</h3>
-        </div>
+        <p class="font-bold text-slate-600 antialiased mt-2">Admin blog</p>
+      </div>
+      <div class="w-full h-fit flex flex-col justify-center items-center">
+        <button
+          id="dashboard"
+          class="nav-btn rounded-md mt-2 hover:bg-blue-500 w-5/6 h-10 transition duration-300 ease-out hover:ease-in hover:text-white"
+          @mouseenter="mouseOver(0)"
+          @mouseout="mouseOut(0)"
+        >
+          <nuxt-link to="/dashboard-admin"
+            class="flex px-2 w-full h-full items-center transition duration-300 ease-out hover:ease-in hover:text-white text-sm">
+            <div class="flex justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                :class="`feather feather-home ${dashboard == true ? 'stroke-white' : 'stroke-black'} mr-2`">
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+              </svg>
+              Dashboard
+            </div>
+          </nuxt-link>
+        </button>
+        <button
+        id="profile"
+          class="nav-btn rounded-md mt-2 hover:bg-blue-500 w-5/6 h-10 transition duration-300 ease-out hover:ease-in hover:text-white"
+          @mouseenter="mouseOver(1)"
+          @mouseout="mouseOut(1)"
+        >
+          <nuxt-link to="/dashboard-admin/profile"
+            class="flex px-2 w-full h-full transition duration-300 ease-out hover:ease-in hover:text-white text-sm">
+            <div class="flex justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                :class="`feather feather-user ${profile == true ? 'stroke-white' : 'stroke-black'} mr-2`">
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+              Profile
+            </div>
+          </nuxt-link>
+        </button>
+        <button
+        id="manage"
+          class="nav-btn rounded-md mt-2 hover:bg-blue-500 w-5/6 h-10 transition duration-300 ease-out hover:ease-in hover:text-white"
+          @mouseover="mouseOver(2)"
+          @mouseout="mouseOut(2)"
+        >
+          <nuxt-link to="/dashboard-admin/create-blog"
+            class="flex px-2 w-full h-full transition duration-300 ease-out hover:ease-in hover:text-white text-sm">
+            <div class="flex justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                :class="`feather feather-edit ${manage == true ? 'stroke-white' : 'stroke-black'} mr-2`">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+              </svg>
+              Manage Blog
+            </div>
+          </nuxt-link>
+        </button>
       </div>
     </div>
   </div>
@@ -50,12 +99,38 @@
     data() {
       return {
         showMenu: false,
+        dashboard: false,
+        profile: false,
+        manage: false,
+        active: false
       }
     },
     methods: {
       toggleNavbar: function () {
         this.showMenu = !this.showMenu;
       },
+      mouseOver(i) {
+        if (i == 0) {
+          this.dashboard = true
+        }
+        if (i == 1) {
+          this.profile = true
+        }
+        if (i == 2) {
+          this.manage = true
+        }
+      },
+      mouseOut(i) {
+        if (i == 0) {
+          this.dashboard = false
+        }
+        if (i == 1) {
+          this.profile = false
+        }
+        if (i == 2) {
+          this.manage = false
+        }
+      }
     }
   }
 
@@ -64,7 +139,8 @@
   .sidebar {
     height: 100vh;
     overflow: hidden;
-    background: #041721;
+    background: #FFFF;
+    font-family: 'Inter', sans-serif;
   }
 
 </style>
---
+++ pages/dashboard-admin/article/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="mr-4 w-full shadow-md my-8 px-5 py-5 relative bg-white rounded-xl">
+    <h1 class="text-md font-semibold">
+      Article & Blog
+    </h1>
+    <p class="text-sm font-normal text-gray-400">
+      Daftar Artikel & blog
+    </p>
+
+    <div>
+      <div class="my-4">
+        <button type="submit"
+          class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+          Add Article
+        </button>
+      </div>
+    </div>
+
+    <div class="overflow-x-auto relative my-8">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="py-3 px-6">
+              #
+            </th>
+            <th scope="col" class="py-3 px-6">
+              Title
+            </th>
+            <th scope="col" class="py-3 px-6">
+              Category
+            </th>
+            <th scope="col" class="py-3 px-6">
+              Author
+            </th>
+            <th scope="col" class="py-3 px-6">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              1
+            </th>
+            <td class="py-4 px-6">
+              Title Article/Blog
+            </td>
+            <td class="py-4 px-6">
+              Category
+            </td>
+            <td class="py-4 px-6">
+              Physician
+            </td>
+            <td class="py-4 px-6">
+              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a> |
+              <a href="#" class="font-medium text-red-600 dark:text-blue-500 hover:underline">Delete</a>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AdminBlogIndex",
+    layout: "Admin",
+  }
+
+</script>
