19dbfe501ae109c6249df4949ed209b7a1ac5fcf
siplancelot <sip.lancelot@gmail.com>
2022-11-16 13:52:43 +0700

    update view blog detail

--- pages/blog/_detail.vue
+++ pages/blog/_detail.vue
@@ -10,21 +10,112 @@
         <p class="text-xs font-semibold text-gray-400">{{ $moment(detail.DatePublish).format('DD MM YYYY') }}</p>
       </div>
     </div>
+    <div class="my-2">
+      <p class="text-sm font-semibold">Kesehatan, COVID-19</p>
+    </div>
     <div class="w-full">
       <img src="../../assets/images/search_bg.png" class="w-full" alt="">
     </div>
     <div class="content my-4" v-html="detail.Content"></div>
-
+    <div class="my-3">
+      <p class="text-sm font-semibold">Tag</p>
+      <div class="flex my-2">
+        <span
+          class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+        <span
+          class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+        <span
+          class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+        <span
+          class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Default</span>
+      </div>
+    </div>
+    <div class="my-8">
+      <h2 class="text-2xl font-semibold my-4">Related Article</h2>
+      <div class="grid grid-cols-3">
+        <div
+          class="blog-items mx-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <img class="rounded-t-lg w-full" src="../../assets/images/search_bg.png" alt="">
+          <div class="blog-content p-5">
+            <h5 class="mb-1 text-lg font-bold tracking-tight text-gray-900 dark:text-white">Judul</h5>
+            <p class="text-gray-700 text-sm font-semibold mb-1">Category</p>
+            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt quas iure, soluta ipsa,
+              accusantium distinctio inventore error maxime beatae, consequuntur accusamus minima tempora delectus
+              labore. Qui recusandae earum facere.</p>
+            <!-- <h4 class="hidden">{{ item.CategoryName }}</h4> -->
+            <!-- <p class="text-content h-24 overflow-hidden mb-3 font-normal text-sm text-gray-700 dark:text-gray-400"
+            v-html="item.Content"></p> -->
+            <NuxtLink to="#">
+              <p
+                class="inline-flex items-center py-2 text-sm font-semibold text-center text-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg aria-hidden="true" class="ml-2 -mr-1 mt-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+                </svg>
+              </p>
+            </NuxtLink>
+          </div>
+        </div>
+        <div
+          class="blog-items mx-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <img class="rounded-t-lg w-full" src="../../assets/images/search_bg.png" alt="">
+          <div class="blog-content p-5">
+            <h5 class="mb-1 text-lg font-bold tracking-tight text-gray-900 dark:text-white">Judul</h5>
+            <p class="text-gray-700 text-sm font-semibold mb-1">Category</p>
+            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt quas iure, soluta ipsa,
+              accusantium distinctio inventore error maxime beatae, consequuntur accusamus minima tempora delectus
+              labore. Qui recusandae earum facere.</p>
+            <!-- <h4 class="hidden">{{ item.CategoryName }}</h4> -->
+            <!-- <p class="text-content h-24 overflow-hidden mb-3 font-normal text-sm text-gray-700 dark:text-gray-400"
+            v-html="item.Content"></p> -->
+            <NuxtLink to="#">
+              <p
+                class="inline-flex items-center py-2 text-sm font-semibold text-center text-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg aria-hidden="true" class="ml-2 -mr-1 mt-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+                </svg>
+              </p>
+            </NuxtLink>
+          </div>
+        </div>
+        <div
+          class="blog-items mx-2 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <img class="rounded-t-lg w-full" src="../../assets/images/search_bg.png" alt="">
+          <div class="blog-content p-5">
+            <h5 class="mb-1 text-lg font-bold tracking-tight text-gray-900 dark:text-white">Judul</h5>
+            <p class="text-gray-700 text-sm font-semibold mb-1">Category</p>
+            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt quas iure, soluta ipsa,
+              accusantium distinctio inventore error maxime beatae, consequuntur accusamus minima tempora delectus
+              labore. Qui recusandae earum facere.</p>
+            <!-- <h4 class="hidden">{{ item.CategoryName }}</h4> -->
+            <!-- <p class="text-content h-24 overflow-hidden mb-3 font-normal text-sm text-gray-700 dark:text-gray-400"
+            v-html="item.Content"></p> -->
+            <NuxtLink to="#">
+              <p
+                class="inline-flex items-center py-2 text-sm font-semibold text-center text-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg aria-hidden="true" class="ml-2 -mr-1 mt-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+                </svg>
+              </p>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="my-8">
       <h2 class="text-2xl font-semibold my-4">Add Comment</h2>
       <form action="#">
-        <div class="mb-6">
-          <label for="default-input"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
-          <input type="text" id="default-input"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Enter your name...">
-        </div>
         <div class="mb-6">
           <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Comment</label>
           <textarea id="message" rows="4"
@@ -96,7 +187,7 @@
     name: 'DetailPage',
     layout: 'Layout',
     computed: {
-      detail () {
+      detail() {
         return this.$store.state.blog.viewBlog
       }
     },
